11d9e8c442715565ebc1fd5d15027815b26b08d4
Tibowl <Tibowl@users.noreply.github.com>
2024-11-11 00:00:00 +0000

    v1.2.0

---
+++ enums/CatAnimeModifier/OverrideAnime.cs
@@ -0,0 +1,10 @@
+
+// Namespace: 
+public enum CatAnimeModifier.OverrideAnime 
+{
+	// Fields
+	public int value__; // 0x0
+	public const CatAnimeModifier.OverrideAnime None = 0;
+	public const CatAnimeModifier.OverrideAnime Gomenne = 1;
+	public const CatAnimeModifier.OverrideAnime Grooming = 2;
+}
