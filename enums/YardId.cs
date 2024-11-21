589f440ec3d50fe7ecec864eb4e11bc07cc337c6
Tibowl <Tibowl@users.noreply.github.com>
2024-11-21 00:00:00 +0000

    v1.3.1

--- enums/YardId.cs
+++ enums/YardId.cs
@@ -4,6 +4,7 @@ public enum YardId
 {
 	// Fields
 	public int value__; // 0x0
+	public const YardId Unknown = -1;
 	public const YardId Usual1 = 0;
 	public const YardId Usual2 = 1;
 	public const YardId Zen = 2;
