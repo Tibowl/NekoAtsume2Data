cea4c13fc6aa011acca7adebddfbd1589ee3535f
Tibowl <Tibowl@users.noreply.github.com>
2025-02-13 00:00:00 +0000

    v1.12.0

---
+++ enums/App/JohoyaPresentId.cs
@@ -0,0 +1,11 @@
+
+// Namespace: App
+public enum JohoyaPresentId 
+{
+	// Fields
+	public int value__; // 0x0
+	public const JohoyaPresentId Silver10 = 0;
+	public const JohoyaPresentId Gold1 = 1;
+	public const JohoyaPresentId GoodKarikari = 2;
+	public const JohoyaPresentId NekoCan = 3;
+}
---
+++ enums/App/LetterRewardId.cs
@@ -0,0 +1,10 @@
+
+// Namespace: App
+public enum LetterRewardId 
+{
+	// Fields
+	public int value__; // 0x0
+	public const LetterRewardId Nekocan = 0;
+	public const LetterRewardId Ticket = 1;
+	public const LetterRewardId Funamori = 2;
+}
